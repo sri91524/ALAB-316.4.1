ca91286b747b8b76459f697e3d8b799ba9f9b834
sri91524 <sreeravish@gmail.com>
2025-02-07 13:02:21 -0500

    Completed Part3

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ console.log(form);
 // The username cannot contain any special characters or whitespace.
 const username = form.elements.username;
 const email = form.elements.email;
+const password = form.elements.password;
+const passwordCheck = form.elements.passwordCheck;
+let inputUserName = "";
 
 const div = document.getElementById("errorDisplay");
 const ul = document.createElement("ul");
@@ -15,7 +18,7 @@ div.appendChild(ul);
 let strErrMsg = "";
 
 username.addEventListener("change", function(e){
-    const inputUserName= e.target.value;
+    inputUserName= e.target.value;
     const regex = /^[A-Za-z0-9]+$/;  
     
     strErrMsg = "The username cannot contain any special characters or whitespace.";
@@ -28,8 +31,7 @@ username.addEventListener("change", function(e){
     {
         removeErrMsgIfExist(strErrMsg);
     }    
-    showErrorDialog();
-
+ 
     //Set removes duplicates
     const uniqueCharacters = new Set(inputUserName);
     strErrMsg = "The username must contain at least two unique characters."; 
@@ -62,8 +64,7 @@ email.addEventListener("change",function(e){
     {        
         removeErrMsgIfExist(strErrMsg);
     }
-    showErrorDialog();
-
+  
     strErrMsg = "The email must not be from the domain 'example.com.'";
     if(inputEmail.substring(inputEmail.indexOf('@')+1).trim().toLowerCase() ===  strDomain.trim().toLowerCase()){        
         checkErrMsgDisplay(strErrMsg);
@@ -77,17 +78,154 @@ email.addEventListener("change",function(e){
 
 });
 
+//ToDo Part3 --3 -- Registration Form - Password Validation:
+// Passwords must be at least 12 characters long.
+// Passwords must have at least one uppercase and one lowercase letter.
+// Passwords must contain at least one number.
+// Passwords must contain at least one special character.
+// Passwords cannot contain the word "password" (uppercase, lowercase, or mixed).
+// Passwords cannot contain the username.
+// Both passwords must match.
+password.addEventListener("change", function(e){
+    const inputPassword = e.target.value; 
+    ValidatePassword(inputPassword, "password");    
+});
+
+passwordCheck.addEventListener("change", function(e){
+    const confirmPassword = e.target.value;
+    ValidatePassword(confirmPassword,"passwordcheck");
+});
+
+// ToDo -- Part 5--Registration Form - Form Submission:
+// Usually, we would send this information to an external API for processing. In our case, we are going to process and store the data locally for practice purposes.
+// If all validation is successful, store the username, email, and password using localStorage.
+// If you are unfamiliar with localStorage, that is okay! Reference the documentation's "Description" and "Examples" sections to learn how to implement it. If you run into issues speak with a peer or one of your instructors.
+// Consider how you want to store the user data, keeping in mind that there will be quite a few users registering for the site. Perhaps you want to store it with an array of user objects; or maybe an object whose keys are the usernames themselves.
+// Valid usernames should be converted to all lowercase before being stored.
+// Valid emails should be converted to all lowercase before being stored.
+// Clear all form fields after successful submission and show a success message.
+
+form.addEventListener("submit", ValidateRegistrationForm);
+
+// ToDo -- part 6--Registration Form - Username Validation (Part Two):
+// Now that we are storing usernames, create an additional validation rule for them...
+// Usernames must be unique ("that username is already taken" error). Remember that usernames are being stored all lowercase, so "learner" and "Learner" are not unique.
 
 
 //-------Helper Functions------------------------
+function ValidateRegistrationForm(e){   
+
+    e.preventDefault();
+    if(ul.hasChildNodes())
+    {
+        showErrorDialog(); 
+        return false;
+    }
+    else{
+        let usrName = username.value.trim().toLowerCase();
+        let pwd = password.value.trim();
+        let usrEmail = email.value.trim().toLowerCase();    
+        let users = {username:usrName, password:pwd, email:usrEmail};
+        // console.log(users);
+
+        if(localStorage.getItem(usrName))
+        {
+            div.textContent ="Sorry, Username already taken!"; 
+        }
+        else
+        {
+            localStorage.setItem(usrName, JSON.stringify(users));               
+            form.reset(); 
+            div.textContent = "Registration Successful!!";
+            div.style.color = "green";
+        }        
+        div.style.display ="block";         
+        setTimeout(() => {
+            div.style.display = "none";
+            div.textContent = "";
+        },5000)
+    }
+    return true;
+}
+
+function ValidatePassword(Password, passwordType)
+{
+    const strPassword = "password";
+    const passwordVal = password.value.trim();
+    const confirmPasswordVal = passwordCheck.value.trim();
+
+    strErrMsg = "Password must have at least one uppercase, one lowercase letter, one number and one special character."; 
+    if(!isValidPassword(Password))
+    {
+        checkErrMsgDisplay(strErrMsg);
+
+        if(passwordType === "password")
+        {password.focus();}
+        else{passwordCheck.focus();}        
+    }
+    else
+    {        
+        removeErrMsgIfExist(strErrMsg);
+    }
+     
+    strErrMsg = "Password cannot contain the word 'password' (uppercase, lowercase, or mixed).";   
+    if(Password.trim().toLowerCase().indexOf(strPassword.trim().toLowerCase()) >= 0)
+    {
+        checkErrMsgDisplay(strErrMsg);
+
+        if(passwordType === "password")
+        {password.focus();}
+        else{passwordCheck.focus();}  
+    }
+    else
+    {        
+        removeErrMsgIfExist(strErrMsg);
+    }
+ 
+    strErrMsg = "Password cannot contain the username.";     
+    if(Password.trim().toLowerCase().indexOf(inputUserName.trim().toLowerCase()) >= 0)
+    {
+        checkErrMsgDisplay(strErrMsg);
+
+        if(passwordType === "password")
+        {password.focus();}
+        else{passwordCheck.focus();}  
+    }
+    else
+    {        
+        removeErrMsgIfExist(strErrMsg);
+    }
+
+    strErrMsg = "Both passwords must match.";
+    if(confirmPasswordVal.trim() != ""){
+        if(passwordVal.trim().toLowerCase() !== confirmPasswordVal.trim().toLowerCase())
+        {checkErrMsgDisplay(strErrMsg);
+    
+            if(passwordType === "password")
+            {password.focus();}
+            else{passwordCheck.focus();}  
+        }
+        else
+        {        
+            removeErrMsgIfExist(strErrMsg);
+        }
+    }   
+    showErrorDialog(); 
+}
+
+function isValidPassword(password)
+{
+    const pattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*()|:<>.,?]).+$");
+    return pattern.test(password);
+}
+
 function isValidEmail(email) {
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return regex.test(email);
 }
 
 function showErrorDialog()
-{console.log("childnode", ul.hasChildNodes());
-    
+{    
     if(ul.hasChildNodes())
     {
         div.style.display = "block"; 
